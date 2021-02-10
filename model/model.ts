ebe89195c494e802d24050e61657e48ba4c06e02
Eric Camellini <eric.camellini@gmail.com>
2021-02-10 12:34:14 +0100

    Committing sample website after tutorial

---
+++ model/model.ts
@@ -0,0 +1,12 @@
+export type PostData = {
+  id: string;
+  title: string;
+  date: Date;
+};
+
+export type PostDataWContent = {
+  id: string;
+  title: string;
+  date: Date;
+  content: string;
+};
---
+++ pages/posts/[id].tsx
@@ -0,0 +1,45 @@
+import Head from "next/head";
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import { PostDataWContent } from "../../model/model";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.scss";
+
+type Props = {
+  postData: PostDataWContent;
+};
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export default function Post(props: Props) {
+  const { postData } = props;
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.content }} />
+      </article>
+    </Layout>
+  );
+}
